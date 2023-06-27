03afbd02a9a1e5d5f994566fa3f56bf82760dc5d
zahide <108192527+zahideuzun@users.noreply.github.com>
2023-06-28 02:54:45 +0300

    Add project files.

---
+++ UserApp.DAL/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserApp.DAL.Entities
+{
+	public class User
+	{
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public string Email { get; set; }
+		public string PhoneNumber { get; set; }
+
+		//public IFormFile Image { get; set; } 
+
+
+
+	}
+}
